b575a727d89bd7f6f08497a957abbaf09dd7041c
tane-git <tanelives@gmail.com>
2022-02-05 20:33:29 +1300

    add external css links on portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,9 +2,17 @@ import About from "../components/About";
 import Header from "../components/Header";
 import Projects from "../components/Projects";
 
+import Head from 'next/head'
+
 export default function portfolio() {
   return (
     <div className='app'>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet"/>
+        <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'/>
+      </Head>
       {/* <h1>Portfolio?</h1> */}
       <Header/>
       <About/>
